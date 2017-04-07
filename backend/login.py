62c8ff18936da9fbe6ae6aad871efc95c0f2c5a8
vincenzodipietro <vincenzodipietromail@gmail.com>
2017-04-07 09:45:25 +0200

    some fix

--- backend/login.py
+++ backend/login.py
@@ -1,4 +1,5 @@
-#from flask import Flask, request, render_template
+#from flask import Flask
+from flask import render_template, url_for, redirect
 #app = Flask(__name__)
 
 #@app.route("/login", methods=['GET', 'POST'])
@@ -6,5 +7,7 @@
 def login(request, session):
     if 'user_id' not in session:
         if request.method=='POST' :
-            session['user_id'] = 1 
+            session['user_id'] = 1
+        else:
+            return render_temeplate("login.html")
     return redirect(url_for('home'))
