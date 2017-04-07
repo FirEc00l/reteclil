c858be1762f8d3bbbf43e4f08fea0dbb2f50fc4f
vincenzodipietro <vincenzodipietromail@gmail.com>
2017-04-07 08:41:47 +0200

    login aggiornato manca la parte di gafrau

--- backend/login.py
+++ backend/login.py
@@ -1,2 +1,14 @@
 #controllare se la richiesta è post, se lo è fare login 
 #altrimenti renderizzare il template login.html
+
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route("/login")
+
+def login():
+    if methods=='POST' :
+        #garau deve fare il login in sql ciao
+       
+    else :
+         return render_template("login.html")
