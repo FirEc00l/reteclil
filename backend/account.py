135028fb178042d93b1200cd0cf3fb5deeb1b75b
Alessandro capici <alessandrocapici.ac@gmail.com>
2017-04-12 13:45:33 +0200

    add database and chanche mail and psw

--- backend/account.py
+++ backend/account.py
@@ -10,12 +10,42 @@ from flask import render_tempalte
 #@app.route('/')
 
 def account(request, session):
+    db = pysqlite3()
     if 'user_id' in session:
         if request.method=='POST' :
-            
-            
+            if request.action=='password':
+                query = """SELECT User.password
+                        FROM User
+                        WHERE User.id_user=%s
+                        """ % session['user_id']
+                result=db.query_db(query)
+                password=result[0][4]
+                if password==request.form['oldpassword']:
+                    newPassword=request.form['newpassword']
+                    query = """UPDATE User
+                        SET password=%s
+                        WHERE User.id_user=%s
+                        """ % newPassword, session['user_id']
+                else:
+                    #errore
+            if request.action=='email':
+                query = """SELECT User.email
+                        FROM User
+                        WHERE User.id_user=%s
+                        """ % session['user_id']
+                result=db.query_db(query)
+                mail=result[0][6]
+                if mail==request.form['oldemail']:
+                    newMail=request.form['newemail']
+                    query = """UPDATE User
+                        SET email=%s
+                        WHERE User.id_user=%s
+                        """ % newMail, session['user_id']
+                else:
+                    ##errore
+                    
+                
         else:
-            db = pysqlite3()
             query = """SELECT User.email
                         FROM User
                         WHERE User.id_user=%s
