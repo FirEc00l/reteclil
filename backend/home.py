d62f850a2c343a852a4947b45acfdf1effc5680a
Sal0hc1n <sollsharp@gmail.com>
2017-04-19 13:11:49 +0200

    passed Loged to render_template

--- backend/home.py
+++ backend/home.py
@@ -1,8 +1,8 @@
 '''
 home.py
 @author: Nicholas Sollazzo
-@version: 1.4
-@date: 7/04/17
+@version: 1.5
+@date: 19/04/17
 '''
 from flask import render_template
 import json
@@ -21,4 +21,4 @@ def home(session):
         description = json.loads(data_str)['description']
 
 
-    return render_template('home.html', links=links, description=description)
+    return render_template('home.html', links=links, description=description, logged=logged)
