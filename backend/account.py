ab3cb7ab61494d12e001761084e59b0acfda2a03
Alessandro capici <alessandrocapici.ac@gmail.com>
2017-04-19 13:05:24 +0200

    forse ho finito

--- backend/account.py
+++ backend/account.py
@@ -6,40 +6,43 @@ from flask import render_tempalte
 def account(request, session):
     db = pysqlite3()
     if 'user_id' in session:
-         query = """SELECT email
+        
+        query = """SELECT password
                         FROM User
-                        WHERE id_user=%s
+                        WHERE id_user="%s"
                         """ % session['user_id']
-            result=db.query_db(query)
-            mail=result[0][6]
-            
+        result=db.query_db(query)
+        password=result[0][4]
+        
+        query = """SELECT email
+                        FROM User
+                        WHERE id_user="%s"
+                        """ % session['user_id']
+        result=db.query_db(query)
+        mail=result[0][6]
+
         if request.method=='POST' :
             if request.action=='password':
-                query = """SELECT password
-                        FROM User
-                        WHERE id_user=%s
-                        """ % session['user_id']
-                result=db.query_db(query)
-                password=result[0][4]
                 if password==request.form['oldpassword']:
                     newPassword=request.form['newpassword']
                     query = """UPDATE User
-                        SET password=%s
-                        WHERE id_user=%s
+                        SET password="%s"
+                        WHERE id_user="%s"
                         """ % newPassword, session['user_id']
+                    return render_template("acoount.html",email=mail)
                 else:
                      return render_template("acoount.html",email=mail,error="Password errata")
             if request.action=='email':
-                if mail==request.form['oldemail']:
+                if password==request.form['password'] :
                     newMail=request.form['newemail']
                     query = """UPDATE User
-                        SET email=%s
-                        WHERE User.id_user=%s
+                        SET email="%s"
+                        WHERE User.id_user="%s"
                         """ % newMail, session['user_id']
+                    return render_template("acoount.html",email=mail)
                 else:
-                   return render_template("acoount.html",email=mail,error="M errata"
+                   return render_template("acoount.html",email=mail,error="Password errata")
                     
-                
         else:
             return render_template("acoount.html",email=mail)
             
