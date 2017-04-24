d86d7e05cbaac22d34832e47c743d8b79b08122f
salvatore giammanco <salvatore giammanco>
2017-04-24 18:12:32 +0200

    Added app secret key in init

--- __init__.py
+++ __init__.py
@@ -59,5 +59,6 @@ def route_thread():
 def route_search():
     return search.search(request)
 
+app.secret_key = '\xd7\x9b\xe4\xa2\xa4\x0b\xb5\xd7\xa6}\x1c\xd2\xb6\x1b_\xd9\x12\xdd\xa5\t\xf7\xd5%n'
 if __name__ == "__main__":
     app.run()
