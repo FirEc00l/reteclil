6998b8542924c84fa18aafa5e2f83b9c6601a657
salvatore giammanco <salvatore giammanco>
2017-04-24 18:02:36 +0200

    Upload.html almost done

--- backend/upload.py
+++ backend/upload.py
@@ -1,11 +1,25 @@
+import backend.clil_utils.db as utils
 from  flask import render_template, url_for, redirect, abort
 
 def upload(request, session):
-	if 'user_id' not in session:
+	if 'user_id' not in session: #togliere not
+		db =  utils.pysqlite3()
 		if request.method == 'POST':
 			pass
 		else:
-			sections = ["Section 1", "Section 2", "Section 3"] #Prenderle dal db
-			return render_template("upload.html", sections=sections)
+			query = "SELECT section_name, id_section FROM section"
+			sections = db.query_db(query)
+			print sections
+			sections_dict = []
+			for section in sections:
+				query = """SELECT sub_name, id_sub
+				FROM sub_section
+				WHERE id_section="%s\"""" % str(section[1])
+				result = db.query_db(query)
+				sections_dict.append( {'name': section[0], 'list': result} )
+
+			sections = sections_dict
+			print sections
+			return render_template("upload.html", sections=sections, logged=True)
 	else:
 		abort(403)
