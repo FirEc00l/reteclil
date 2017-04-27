b3ab6ee9d0373c6f1c27cff3ae050b32f2cf359b
Alessandro capici <alessandrocapici.ac@gmail.com>
2017-04-27 10:00:18 +0200

    modifica pass riuscita , modificato html account (errore di scrittura)

--- backend/account.py
+++ backend/account.py
@@ -1,5 +1,5 @@
 from flask import render_template
-import clil_utils.db as utils
+import backend.clil_utils.db as utils
 
 
 def account(request, session):
@@ -7,36 +7,38 @@ def account(request, session):
     if 'user_id' in session:
         query = """SELECT password
                         FROM User
-                        WHERE id_user="%s"
+                        WHERE id_user=%s
                         """ % session['user_id']
         result=db.query_db(query)
-        password=result[0][4]
+        password=result[0][0]
         
         query = """SELECT email
                         FROM User
-                        WHERE id_user="%s"
+                        WHERE id_user=%s
                         """ % session['user_id']
         result=db.query_db(query)
-        mail=result[0][6]
+        mail=result[0][0]
 
         if request.method=='POST' :
-            if request.action=='password':
+            if request.form['action']=='password':
                 if password==request.form['oldpassword']:
                     newPassword=request.form['newpassword']
                     query = """UPDATE User
                         SET password="%s"
-                        WHERE id_user="%s"
-                        """ % newPassword, session['user_id']
+                        WHERE id_user=%s
+                        """ % (newPassword, session['user_id'])
+                    db.query_db(query)
                     return render_template("account.html",email=mail)
                 else:
                     return render_template("account.html",email=mail,error="Password errata")
-            if request.action=='email':
+            if request.form['action']=='email':
                 if password==request.form['password']:
                     newMail=request.form['newemail']
                     query = """UPDATE User
                         SET email="%s"
-                        WHERE User.id_user="%s"
-                        """ % newMail, session['user_id']
+                        WHERE User.id_user=%s
+                        """ % (newMail, session['user_id'])
+                    db.query_db(query)
                     return render_template("account.html",email=mail)
                 else:
                    return render_template("account.html",email=mail,error="Password errata")
@@ -47,9 +49,9 @@ def account(request, session):
     else:
     	query = """SELECT email
                         FROM User
-                        WHERE id_user="%s"
+                        WHERE id_user=%s
                         """ % session['user_id']
         result=db.query_db(query)
-        mail=result[0][6]
+        mail=result[0][0]
         return render_template("account.html",email=mail)
        
