6e753adece4ed43f5ef898c5517157f93027977e
Garau <cristian.garau7@gmail.com>
2017-05-03 12:56:51 +0200

    Modificato nome del file nel db, e search.py

--- __init__.py
+++ __init__.py
@@ -88,7 +88,7 @@ def route_search():
     return search.search(request, session)
 
 @app.route("/search/<search_key>", methods = ['GET','POST'])
-def route_search_key():
+def route_search_key(search_key):
     return search.search(request, session, search_key)
 
 app.secret_key = '\xd7\x9b\xe4\xa2\xa4\x0b\xb5\xd7\xa6}\x1c\xd2\xb6\x1b_\xd9\x12\xdd\xa5\t\xf7\xd5%n'
