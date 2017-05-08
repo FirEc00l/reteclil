fa2d3b285e3f58b44dd6dbb313a74545fb0d6423
CapoTheBoss <alessandrocapici.ac@gmail.com>
2017-05-08 12:02:20 +0200

    account con psw criptata

--- backend/account.py
+++ backend/account.py
@@ -24,8 +24,7 @@ def account(request, session):
 
         if request.method=='POST' :
             if request.form['action']=='password':
-                Form_hash_psw=generate_password_hash(request.form['oldpassword'])
-                if check_password_hash(password,Form_hash_psw):
+                if check_password_hash(password,request.form['oldpassword']):
                     newPassword=request.form['newpassword']
                     newPassword=generate_password_hash(newPassword)
                     query = """UPDATE User
@@ -37,8 +36,7 @@ def account(request, session):
                 else:
                     return render_template("account.html",email=mail,error="Password errata", logged=logged)
             if request.form['action']=='email':
-                Form_hash_psw=generate_password_hash(request.form['password'])
-                if check_password_hash(password,Form_hash_psw):
+                if check_password_hash(password,request.form['password']):
                     newMail=request.form['newemail']
                     query = """UPDATE User
                         SET email="%s"
