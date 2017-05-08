f0006905c016f86ea36655922874118e9fe207f5
CapoTheBoss <alessandrocapici.ac@gmail.com>
2017-05-08 11:54:46 +0200

    login con psw criptata

--- backend/login.py
+++ backend/login.py
@@ -1,6 +1,8 @@
 #from flask import Flask
 import backend.clil_utils.db as utils
 from flask import render_template, url_for, redirect
+from werkzeug.security import generate_password_hash, \
+     check_password_hash
 #app = Flask(__name__)
 #@app.route("/login", methods=['GET', 'POST'])
 
@@ -16,11 +18,11 @@ def login(request, session):
                        FROM User
                        WHERE User.username="%s\"""" % request.form['username']
             result = db.query_db(query)
-
+            print check_password_hash(request.form['password'],result[0][1])
             if result==None :
                 return render_template("login.html", error = "Nome utente o password errata", logged=False)
-
-            elif request.form['password']==result[0][1] :
+        
+            elif check_password_hash(result[0][1],request.form['password']):
                 session['user_id'] = result[0][2]
                 session['user_type'] = result[0][3]
                 return redirect(url_for('route_home'))
