a6751a37cb0ac06daee5ef23d0fcc9b50a8fb668
salvatore giammanco <salvatore giammanco>
2017-05-08 18:25:31 +0200

    upload.py: some work

--- backend/upload.py
+++ backend/upload.py
@@ -6,8 +6,10 @@ Just kidding, my code is so good that you can understand it without comments
 """
 
 import backend.clil_utils.db as utils
-from  flask import render_template, url_for, redirect, abort
+from  flask import render_template, url_for, redirect, abort, request
 from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 def upload(request, session):
 	if 'user_id' in session:
@@ -21,16 +23,14 @@ def upload(request, session):
 
 		if request.method == 'POST':
 			f = request.files['file']
-			#Controllare estensione, dimensione file
-			ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
 			if f and allowed_file(f.filename):
+				#Controllare nome file nel db, se ce gia aggiungere qualchecos
+				query = "SELECT id_file FROM file WHERE name = '%s'" % f.filename
+				# = db.query_db(query)
 				f.save('./files/' + secure_filename(f.filename))
+				return "success"
 			else:
 				return "error"
-			#Controllare nome file nel db, se ce gia aggiungere qualchecos
-
-
-			return render_template("upload.html", sections=sections)
 		else:
 			sections_dict = []
 			for section in sections:
@@ -41,7 +41,10 @@ def upload(request, session):
 				sections_dict.append( {'name': section[0], 'list': result} )
 
 			sections = sections_dict
-
 			return render_template("upload.html", sections=sections, logged=logged)
 	else:
 		abort(403)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
