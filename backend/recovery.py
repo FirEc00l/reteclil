814b5a9b410d769f2c6023d7527098c80d41b65d
CapoTheBoss <alessandrocapici.ac@gmail.com>
2017-05-12 12:05:53 +0200

    aggiunta reset_password.html e recovery.py da testare

--- backend/recovery.py
+++ backend/recovery.py
@@ -12,6 +12,7 @@ from werkzeug.security import generate_password_hash, \
 from email.mime.text import MIMEText as mt
 # Import smtplib for the actual sending function
 import smtplib
+import time
 import backend.clil_utils.db as utils
 import random
 
@@ -31,6 +32,9 @@ def recovery(request,session,key=None):
                         """ % user
         result=db.query_db(query)
         ReciveMail=result[0][0]
+        time=time.strftime("%H:%M:%S")
+        rand=''.join(random.choice('0123456789ABCDEF') for i in range(5))
+        MailHash=MailHash+time+rand
         MailHash=generate_password_hash(ReciveMail)
 
 
@@ -71,35 +75,37 @@ def recovery(request,session,key=None):
         db.query_db(query)
 
         if key is not None:
-            query = """SELECT username
+            query = """SELECT key
                         FROM user
-                        WHERE key="%s"
-                        """ % MailHash
+                        WHERE username="%s"
+                        """ % user
             result=db.query_db(query)
-            username=result[0][0]
-
-            #creazione nuova psw
-            onetimePSW = ''.join(random.choice('0123456789ABCDEF') for i in range(5))
-            ConetimePSW = onetimePSW
-            print ConetimePSW
-            #inserimento password db
-            onetimePSW=generate_password_hash(onetimePSW)
-            query = """UPDATE User
-                       SET password="%s"
-                       WHERE username="%s"
-                            """ % (onetimePSW,user)
-            db.query_db(query)
-
-            if request.method=='POST' :#modificare richiesta html
-                query = """SELECT username, password, id_user, user_type
-                           FROM User
-                           WHERE User.username="%s\"""" % username
-                result = db.query_db(query)
-
-                if check_password_hash(result[0][1],ConetimePSW):
-                    session['user_id'] = result[0][2]
-                    session['user_type'] = result[0][3]
-                    return redirect(url_for('account.html'),logged=logged,psw=ConetimePSW)
+            if result[0][0]==key:
+                return render_template("reset_password.html")
+                
+    
+##            #creazione nuova psw
+##            onetimePSW = ''.join(random.choice('0123456789ABCDEF') for i in range(5))
+##            ConetimePSW = onetimePSW
+##            print ConetimePSW
+##            #inserimento password db
+##            onetimePSW=generate_password_hash(onetimePSW)
+##            query = """UPDATE User
+##                       SET password="%s"
+##                       WHERE username="%s"
+##                            """ % (onetimePSW,user)
+##            db.query_db(query)
+##
+##            if request.method=='POST' :#modificare richiesta html
+##                query = """SELECT username, password, id_user, user_type
+##                           FROM User
+##                           WHERE User.username="%s\"""" % username
+##                result = db.query_db(query)
+##
+##                if check_password_hash(result[0][1],ConetimePSW):
+##                    session['user_id'] = result[0][2]
+##                    session['user_type'] = result[0][3]
+##                    return redirect(url_for('account.html'),logged=logged,psw=ConetimePSW)
 
         else:
             return render_template("recovery.html",logged=logged)
