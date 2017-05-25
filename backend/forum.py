f01bec087bb1531e3e7df8c096bb9c30e230d764
vincenzodipietro <vincenzodipietromail@gmail.com>
2017-05-25 10:29:56 +0200

    query and list for threads

--- backend/forum.py
+++ backend/forum.py
@@ -8,6 +8,19 @@ import backend.clil_utils.db as utils
 from flask import render_template, url_for, redirect
 
 def forum(request, session):
-    logged = False;
+    if 'user_id' in session:
+        logged = session['user_type']
 
-    return render_template("forum.html", logged = logged)
+    result_dict = []
+    
+    db =  utils.pysqlite3()
+    query = "SELECT * FROM post"
+    result = db.query_db(query)
+
+    for thread in result:
+                    filename = sub_section[0]
+                    file_format = filename.split(".")[-1]
+                    result_dict.append( {'id_post': thread[0], 'title' : thread[1], 'content' : thread[2], 'date_post' : thread[3], 'id_user' : thread[4], 'id_thread' : thread[5], 'extension': file_format} )
+    threads = result_dict 
+
+    return render_template("forum.html", logged = logged, threads)
