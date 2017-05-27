1cb29ab9817b92858a9a2cde8d59998b22c44a44
vincenzodipietro <vincenzodipietromail@gmail.com>
2017-05-27 10:20:15 +0200

    worka forse?

--- backend/forum.py
+++ backend/forum.py
@@ -10,27 +10,34 @@ from flask import render_template, url_for, redirect
 def forum(request, session):
     if 'user_id' in session:
         logged = session['user_type']
-
     else :
-        logged = None
+        logged = False
 
     result_dict = []
 
     db =  utils.pysqlite3()
-    query = "SELECT * FROM post"
+    query = "SELECT id_thread, title, id_user, name, surname FROM thread NATURAL JOIN user"
     result = db.query_db(query)
 
     print result
 
-    if result == None :
+    if result is None :
         threads = None
         return render_template("forum.html", logged=logged, threads=threads)
 
     else:
         for thread in result:
-                        filename = sub_section[0]
-                        file_format = filename.split(".")[-1]
-                        result_dict.append( {'id_post': thread[0], 'title' : thread[1], 'content' : thread[2], 'date_post' : thread[3], 'id_user' : thread[4], 'id_thread' : thread[5], 'extension': file_format} )
+                        result_dict.append( {'id_thread': thread[0], 'title' : thread[1], 'id_user' : thread[2], 'name' : thread[3]} )
         threads = result_dict
 
+        print thread
+
         return render_template("forum.html", logged=logged, threads=threads)
+
+    '''
+    if request.method=='POST' :
+
+    query = """INSERT INTO table_name
+               VALUES (NULL, %s, , ,%s);""" %
+    
+    '''
