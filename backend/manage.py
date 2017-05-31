f9e43b49726bbe277d8870afbe5cb02b6d29bc27
Sal0hc1n <sollsharp@gmail.com>
2017-05-31 13:36:39 +0200

    manage.py: fixed 'delete_section'

--- backend/manage.py
+++ backend/manage.py
@@ -2,7 +2,7 @@
 manage.py
 @author: Nicholas Sollazzo
 @mail: sollsharp@gmail.com
-@version: 1.5
+@version: 1.5.1
 @date: 31/05/17
 ===============================================
 manage(request):
@@ -200,13 +200,21 @@ def manage(request, session):
 
     # XXX: 7
     def delete_section():
-        section_name = request.form['section_name']
+        id_section = request.form['id_section']
 
-        query = ''' DELETE section WHERE section_name = "{}"; '''.format(
-            section_name)
+        query = '''SELECT COUNT(id_sub) FROM sub_section WHERE id_section = "{}" '''.format(
+            id_section)
 
-        DB.query_db(query)
-        DB.close_db()
+        subs = int(DB.query_db(query))
+
+        if subs > 0:
+            setResult('subs')
+        else:
+            query = '''DELETE FROM section WHERE section_name = "{}"; '''.format(
+                section_name)
+
+            DB.query_db(query)
+            DB.close_db()
 
     # XXX: 8
     def delete_sub_section():
@@ -218,10 +226,9 @@ def manage(request, session):
         files = int(DB.query_db(query))
 
         if files > 0:
-            # TODO implementare in manage.html, cambiare con numeri
             setResult('files')
         else:
-            query = ''' DELETE sub_section WHERE sub_name = "{}"; '''.format(
+            query = '''DELETE FROM sub_section WHERE sub_name = "{}"; '''.format(
                 sub_section_name)
 
         DB.close_db()
